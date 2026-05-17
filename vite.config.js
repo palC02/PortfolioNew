1ad59acb66d767278ac114ec5ad1c4847d2e26f6
CHAYAN PAL <120254873+palC02@users.noreply.github.com>
2026-05-17 22:52:44 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/PortfolioNew/',
-
-export default defineConfig({
-  plugins: [react()],
   server: { 
     port: 3000,
     watch: {
